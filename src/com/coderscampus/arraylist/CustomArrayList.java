bbf53a23443abbbb986ef1dc56d5586164214883
Tyapaevair <tyapaevair@gmail.com>
2022-03-29 10:26:27 -0400

    Updated code

--- src/com/coderscampus/arraylist/CustomArrayList.java
+++ src/com/coderscampus/arraylist/CustomArrayList.java
@@ -1,42 +1,39 @@
 package com.coderscampus.arraylist;
 
 public class CustomArrayList<T> implements CustomList<T> {
-	 Object[] items = new Object[10];
+	Object[] items = new Object[10];
+	int itemSize = 0;
 
-	 @Override
-	 public boolean add(T item) {
-		  for(int i = 0; i < items.length; i++) {
-				if(items[i] == null) {
-					 items[i] = item;
+	@Override
+	public boolean add(T item) {
 
-					 return true;
-				}
+		if (itemSize == items.length) {
+			items = increaseSize();
+		}
 
-		  }
-		  Object[] tempArray = new Object[items.length * 2];
-		  System.arraycopy(items, 0, tempArray, 0, items.length);
-		  items = tempArray;
-		  for(int i = 0; i < items.length; i++) {
-				if(items[i] == null) {
-					 items[i] = item;
+		items[itemSize] = item;
+		itemSize++;
 
-				}
-		  }
-		  return true;
-	 }
+		return true;
+	}
 
-	 @Override
-	 public int getSize() {
+	private Object[] increaseSize() {
+		Object[] tempArray = new Object[itemSize * 2];
+		for (int i = 0; i < itemSize; i++) {
+			tempArray[i] = items[i];
+		}
+		return tempArray;
+	}
 
-		  return items.length;
-	 }
+	@Override
+	public int getSize() {
 
-	 @Override
-	 public T get(int index) {
+		return items.length;
+	}
 
-		  return (T) items[index];
-	 }
+	@Override
+	public T get(int index) {
+
+		return (T) items[index];
+	}
 }
-
-
-
--- src/com/coderscampus/arraylist/CustomListApplication.java
+++ src/com/coderscampus/arraylist/CustomListApplication.java
@@ -3,24 +3,17 @@ package com.coderscampus.arraylist;
 public class CustomListApplication {
 	 public static void main(String[] args) {
 
-		  CustomList<String> myCustomList = new CustomArrayList<>();
-		  myCustomList.add("element 1"); // and continue to add another 10, 20 or 40 more elements
-		  myCustomList.add("element 2");
-		  myCustomList.add("element 3");
-		  myCustomList.add("element 4");
-		  myCustomList.add("element 5");
-		  myCustomList.add("element 6");
-		  myCustomList.add("element 7");
-		  myCustomList.add("element 8");
-		  myCustomList.add("element 9");
-		  myCustomList.add("element 10");
-		  myCustomList.add("element 11");
-		  myCustomList.add("element 21");
+		  CustomList<Integer> myNumbers = new CustomArrayList<>();
+		 
+		  for(int i = 1; i<= 40; i++){
+myNumbers.add(i);
+		  }
+
 
 
 		  // then you should validate that all the elements you've inserted actually exist in your data structure
-		  for(int i = 0; i < myCustomList.getSize(); i++) {
-				System.out.println(myCustomList.get(i));
+		  for(int i = 0; i < myNumbers.getSize(); i++) {
+				System.out.println(myNumbers.get(i));
 		  }
 
 	 }
